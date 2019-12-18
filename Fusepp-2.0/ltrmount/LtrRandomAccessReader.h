8bff7c899a00a12e6af9d6c7f004e2ad3d81e909
Sandra Biton <sandra.biton@zerto.com>
2019-12-19 00:45:01 +0200

    Implement random access read

--- Fusepp-2.0/ltrmount/LtrRandomAccessReader.h
+++ Fusepp-2.0/ltrmount/LtrRandomAccessReader.h
@@ -4,13 +4,13 @@
 class LtrRandomAccessReader
 {
 public:
-	LtrRandomAccessReader(std::string repository_path, std::string dom_path, std::string datapool_path);
+	LtrRandomAccessReader(const std::string& repository_path, const std::string& dom_path, const std::string& datapool_path);
 
 	size_t read(char *buf, size_t size, size_t offset);
 
 private:
-	std::string repository_path;
-	std::string dom_path;
-	std::string datapool_path;
+	std::string repository_path_;
+	std::string dom_path_;
+	std::string datapool_path_;
 };
 
--- Fusepp-2.0/ltrmount/main.cpp
+++ Fusepp-2.0/ltrmount/main.cpp
@@ -1,10 +1,17 @@
 
 #include "RootSubFS.h"
+#include "LtrRandomAccessReader.h"
 
 int main()
 {
-	RootSubFS Wrapper(std::string("C:\\temp\\"));
-	Wrapper.readdir("\\general\\2019-09-18__02_06");
+	LtrRandomAccessReader reader(R"(W:)",
+		R"(pools\3479958491418911683\doms\0c1fcb77-1e9e-440a-8c5b-dd03ff898a13)",
+		R"(pools\3479958491418911683\data)"
+	);
+
+	size_t size_to_read = 5*1024*1024;
+	std::vector<char> data(size_to_read);
+	reader.read(data.data(), size_to_read, 0);
 
 	return 0;
 }
